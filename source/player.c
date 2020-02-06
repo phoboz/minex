84163afe731ec7d21f3a9b9f7e93ff75cd47e93f
phoboz <leputa6@hotmail.com>
2020-02-06 22:57:49 +0100

    Added player explode animation

--- source/player.c
+++ source/player.c
@@ -52,100 +52,116 @@ unsigned int move_player(
 
 	if (player->anim.obj.active)
 	{
-		if (player->fire_countdown)
+		if (player->state == PLAYER_STATE_NORMAL)
 		{
-			player->fire_countdown--;
-		}
-
-		check_joysticks();
-		check_buttons();
-
-		if (joystick_1_left())
-		{
-			if (--player->angle == 0)
+			if (player->fire_countdown)
 			{
-				player->angle = 64;
+				player->fire_countdown--;
 			}
 
-			Rot_VL_ab(player->angle, 0, (signed int *) player_up_vec, player->up_vec);
-			player->update_view = 1;
-		}
-		else if (joystick_1_right())
-		{
-			if (++player->angle == 64)
-			{
-				player->angle = 0;
-			}
+			check_joysticks();
+			check_buttons();
 
-			Rot_VL_ab(player->angle, 0, (signed int *) player_up_vec, player->up_vec);
-			player->update_view = 1;
-		}
-
-		if (button_1_4_pressed())
-		{
-			if (!player->fire_countdown)
+			if (joystick_1_left())
 			{
-				bullet = (struct bullet *) bullet_free_list;
-				if (bullet)
+				if (--player->angle == 0)
 				{
-					init_bullet(bullet, 0, 0, PLAYER_BULLET_SPEED);
-					player->fire_countdown = PLAYER_FIRE_TRESHOLD;
-					status |= PLAYER_STATUS_FIRE;
+					player->angle = 64;
 				}
-			}
-		}
 
-		if (button_1_3_held())
-		{
-			if (++player->speed_counter >= PLAYER_ACCELERATE_TRESHOLD)
+				Rot_VL_ab(player->angle, 0, (signed int *) player_up_vec, player->up_vec);
+				player->update_view = 1;
+			}
+			else if (joystick_1_right())
 			{
-				player->speed_counter = 0;
-				if (player->speed < PLAYER_MAX_SPEED)
+				if (++player->angle == 64)
 				{
-					player->speed++;
-					player->anim.base_frame++;
+					player->angle = 0;
+				}
+
+				Rot_VL_ab(player->angle, 0, (signed int *) player_up_vec, player->up_vec);
+				player->update_view = 1;
+			}
+
+			if (button_1_4_pressed())
+			{
+				if (!player->fire_countdown)
+				{
+					bullet = (struct bullet *) bullet_free_list;
+					if (bullet)
+					{
+						init_bullet(bullet, 0, 0, PLAYER_BULLET_SPEED);
+						player->fire_countdown = PLAYER_FIRE_TRESHOLD;
+						status |= PLAYER_STATUS_FIRE;
+					}
 				}
 			}
 
-			if (player->speed == PLAYER_MAX_SPEED)
+			if (button_1_3_held())
 			{
-				if (++player->anim_counter >= PLAYER_ANIM_TRESHOLD)
+				if (++player->speed_counter >= PLAYER_ACCELERATE_TRESHOLD)
+				{
+					player->speed_counter = 0;
+					if (player->speed < PLAYER_MAX_SPEED)
+					{
+						player->speed++;
+						player->anim.base_frame++;
+					}
+				}
+
+				if (player->speed == PLAYER_MAX_SPEED)
+				{
+					if (++player->anim_counter >= PLAYER_FLAME_ANIM_TRESHOLD)
+					{
+						player->anim_counter = 0;
+						if (++player->anim.frame >= PLAYER_FLAME_NUM_FRAMES)
+						{
+							player->anim.frame = 0;
+						}
+					}
+				}
+				else
 				{
 					player->anim_counter = 0;
-					if (++player->anim.frame >= PLAYER_FLAME_NUM_FRAMES)
-					{
-						player->anim.frame = 0;
-					}
+					player->anim.frame = 0;
 				}
 			}
 			else
 			{
-				player->anim_counter = 0;
-				player->anim.frame = 0;
-			}
-		}
-		else
-		{
-			if (++player->speed_counter >= PLAYER_BRAKE_TRESHOLD)
-			{
-				player->speed_counter = 0;
-				if (player->speed > 0)
+				if (++player->speed_counter >= PLAYER_BRAKE_TRESHOLD)
 				{
-					player->speed--;
-					player->anim.base_frame--;
+					player->speed_counter = 0;
+					if (player->speed > 0)
+					{
+						player->speed--;
+						player->anim.base_frame--;
+					}
 				}
 			}
-		}
 
-		if (player->speed)
+			if (player->speed)
+			{
+				wrap_translate(
+					player->rel_pos,
+					player->rel_pos,
+					player->up_vec[0] * player->speed,
+					player->up_vec[1] * player->speed
+					);
+				player->update_view = 1;
+			}
+		}
+		else if (player->state == PLAYER_STATE_EXPLODE)
 		{
-			wrap_translate(
-				player->rel_pos,
-				player->rel_pos,
-				player->up_vec[0] * player->speed,
-				player->up_vec[1] * player->speed
-				);
-			player->update_view = 1;
+			if (++player->anim_counter >= PLAYER_EXPLODE_ANIM_TRESHOLD)
+			{
+				player->anim_counter = 0;
+				if (++player->anim.frame >= PLAYER_EXPLODE_NUM_FRAMES)
+				{
+					player->anim.frame = 0;
+					player->state = PLAYER_STATE_REMOVED;
+					player->anim.obj.active = 0;
+				}
+			}
 		}
 	}
 
@@ -156,7 +172,12 @@ void hit_player(
 	struct player *player
 	)
 {
-	player->anim.obj.active = 0;
+	if (player->state != PLAYER_STATE_EXPLODE)
+	{
+		player->anim_counter = 0;
+		player->anim.base_frame = PLAYER_EXPLODE_FRAME;
+		player->state = PLAYER_STATE_EXPLODE;
+	}
 }
 
 // ***************************************************************************
--- source/player_data.c
+++ source/player_data.c
@@ -85,13 +85,73 @@ static const signed char player_anim_4[]=
 	(signed char) 0xFF, +0x04*BLOW_UP, -0x02*BLOW_UP, // draw, y, x
 	(signed char) 0x01 // endmarker 
 };
+const signed char player_explode_0[]=
+{	(signed char) 0x00, -0x01*BLOW_UP, -0x03*BLOW_UP, // move to y, x
+	(signed char) 0xFF, -0x03*BLOW_UP, +0x00*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x02*BLOW_UP, +0x04*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x01*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x01*BLOW_UP, -0x02*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x04*BLOW_UP, +0x00*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, -0x02*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x02*BLOW_UP, +0x00*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, -0x02*BLOW_UP, +0x00*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, -0x02*BLOW_UP, +0x04*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x03*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x01 // endmarker 
+};
+const signed char player_explode_1[]=
+{	(signed char) 0x00, -0x01*BLOW_UP, -0x03*BLOW_UP, // move to y, x
+	(signed char) 0xFF, -0x03*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x01*BLOW_UP, +0x03*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x01*BLOW_UP, +0x02*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x01*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x03*BLOW_UP, -0x03*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x03*BLOW_UP, +0x00*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, -0x01*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x05*BLOW_UP, +0x00*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, -0x01*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x02*BLOW_UP, +0x05*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x03*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x01 // endmarker 
+};
+const signed char player_explode_2[]=
+{	(signed char) 0x00, -0x03*BLOW_UP, -0x05*BLOW_UP, // move to y, x
+	(signed char) 0xFF, -0x03*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x01*BLOW_UP, +0x02*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x03*BLOW_UP, +0x06*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x01*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x06*BLOW_UP, -0x04*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x02*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, -0x01*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x09*BLOW_UP, -0x02*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x00*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x01*BLOW_UP, +0x08*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x03*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x02 // endmarker 
+};
+const signed char player_explode_3[]=
+{	(signed char) 0x00, -0x05*BLOW_UP, -0x06*BLOW_UP, // move to y, x
+	(signed char) 0xFF, -0x01*BLOW_UP, -0x01*BLOW_UP, // draw, y, x
+	(signed char) 0xFF, +0x00*BLOW_UP, +0x02*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x01*BLOW_UP, +0x08*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x02*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x00, +0x0A*BLOW_UP, -0x04*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, -0x01*BLOW_UP, -0x02*BLOW_UP, // draw, y, x
+	(signed char) 0x00, -0x0A*BLOW_UP, +0x09*BLOW_UP, // mode, y, x
+	(signed char) 0xFF, +0x02*BLOW_UP, +0x01*BLOW_UP, // draw, y, x
+	(signed char) 0x02 // endmarker 
+};
 const signed char * const player_anim[] =
 {
 	player_anim_0,
 	player_anim_1,
 	player_anim_2,
 	player_anim_3,
-	player_anim_4
+	player_anim_4,
+	player_explode_0,
+	player_explode_1,
+	player_explode_2,
+	player_explode_3
 };
 
 // ***************************************************************************
